256973db22fa467d092d66a2736fff12e1085858
nihades <nihaades@gmail.com>
2018-04-14 18:47:10 +0000

    Update views and navigations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
  
  
-  devise_for :users
- root 'welcome#index'
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
+  
+  root 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
  
- 
+  get 'search_stocks', to: 'stocks#search'
+  
+  resources :user_stocks, only: [:create]
  
  
  
